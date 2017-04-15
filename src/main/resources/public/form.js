d51eec45f34dcc308bd2ae3025871291fb009630
MariLiis <mariliisvelner@gmail.com>
2017-04-16 02:10:43 +0300

    Added locale and normal datetime formatting to datetimepicker

--- src/main/resources/public/form.js
+++ src/main/resources/public/form.js
@@ -2,7 +2,23 @@
  * Created by MariLiis on 15.04.2017.
  */
 
-
 $(document).ready(function () {
-    $('#visitDateTime').datetimepicker();
+    $('#visitDateTime').datetimepicker({
+        format:'d.m.Y H:i',
+        i18n:{
+            et:{
+                months:[
+                    'Jaanuar','Veebruar','Märts','Aprill',
+                    'Mai','Juuni','Juuli','August',
+                    'September','Oktoober','November','Detsember'
+                ],
+                dayOfWeek:[
+                    "P", "E", "T", "K", "N", "R", "L"
+                ]
+            }
+        }
+    });
+
+    $.datetimepicker.setLocale('et');
+
 });
