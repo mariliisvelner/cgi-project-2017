32a8bcb34a1c0b8c15c10d6f80615416733d958a
MariLiis <mariliisvelner@gmail.com>
2017-04-16 23:51:46 +0300

    Added new DTO class for querying dentist visit DTOs

---
+++ src/main/java/com/cgi/dentistapp/dto/SearchQueryDTO.java
@@ -0,0 +1,24 @@
+package com.cgi.dentistapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchQueryDTO {
+
+    @Size(max = 50)
+    private String dentistName;
+
+    @Size(max = 50)
+    private String physicianName;
+
+    @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
+    private LocalDateTime visitDateTime;
+}
