50968d5b5f55b52c2c8940b0da107c9ff27fa372
MariLiis <mariliisvelner@gmail.com>
2017-04-17 16:23:15 +0300

    Created new DTO for the visit_details page #19

---
+++ src/main/java/com/cgi/dentistapp/dto/DetailedViewDTO.java
@@ -0,0 +1,28 @@
+package com.cgi.dentistapp.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailedViewDTO {
+    private Long id;
+
+    @Size(min = 1, max = 50)
+    private String dentistName;
+
+    @Size(min = 1, max = 50)
+    private String physicianName;
+
+    @NotNull
+    @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
+    private LocalDateTime visitDateTime;
+}
