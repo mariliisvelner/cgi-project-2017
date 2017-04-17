bce50d2cd04c333c98deb9e059431e94471054d7
MariLiis <mariliisvelner@gmail.com>
2017-04-17 14:17:47 +0300

    Created new DTO class for returning query results #12

---
+++ src/main/java/com/cgi/dentistapp/dto/SearchQueryResultDTO.java
@@ -0,0 +1,21 @@
+package com.cgi.dentistapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchQueryResultDTO {
+
+    private Long id;
+
+    private String dentistName;
+
+    private String physicianName;
+
+    private LocalDateTime visitDateTime;
+}
