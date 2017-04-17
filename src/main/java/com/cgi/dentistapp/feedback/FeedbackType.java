d269ef6cd40be32bc33373cc4ac1cd7b832e0eae
MariLiis <mariliisvelner@gmail.com>
2017-04-17 14:16:01 +0300

    Created new Feedback classes to generate feedback for user input

---
+++ src/main/java/com/cgi/dentistapp/feedback/FeedbackType.java
@@ -0,0 +1,5 @@
+package com.cgi.dentistapp.feedback;
+
+public enum FeedbackType {
+    ERROR, INFO, SUCCESS, WARNING
+}
---
+++ src/main/java/com/cgi/dentistapp/util/FeedbackUtil.java
@@ -0,0 +1,38 @@
+package com.cgi.dentistapp.util;
+
+import com.cgi.dentistapp.feedback.Feedback;
+import com.cgi.dentistapp.feedback.FeedbackType;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedbackUtil {
+
+    /**
+     * Adds feedback list to model. This can be later used via Thymeleaf to generate Bootstrap alert dialogs.
+     * <p>
+     * For example <code>ControllerUtil.setFeedback(FeedbackType.SUCCESS, "Hello world!")</code> will add a list
+     * containing one {@link Feedback} object that can be used to generate
+     * HTML tag <code><div class="alert alert-success">Hello world!</div></code>
+     * <p>
+     *
+     * @param model    to add feedback
+     * @param type     of the feedback
+     * @param feedback as message
+     * @return model which is updated
+     */
+    public static Model setFeedback(Model model, FeedbackType type, String feedback) {
+        if (model.containsAttribute(Feedback.FEEDBACK_VARIABLE_NAME)) {
+            List<Feedback> feedbacks = (List<Feedback>) model.asMap().get(Feedback.FEEDBACK_VARIABLE_NAME);
+            feedbacks.add(new Feedback(feedback, type));
+            model.addAttribute(Feedback.FEEDBACK_VARIABLE_NAME, feedbacks);
+        } else {
+            List<Feedback> lst = new ArrayList<>();
+            lst.add(new Feedback(feedback, type));
+            model.addAttribute(Feedback.FEEDBACK_VARIABLE_NAME, lst);
+        }
+        return model;
+    }
+
+}
