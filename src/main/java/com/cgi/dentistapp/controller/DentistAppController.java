3d643ef24bfd3ecd78a087c23269d4a7be6beef9
MariLiis <mariliisvelner@gmail.com>
2017-04-17 14:25:19 +0300

    Added MessageSource object Users are now directed to the "visit_details" page after pressing a "Vaata" button

--- src/main/java/com/cgi/dentistapp/controller/DentistAppController.java
+++ src/main/java/com/cgi/dentistapp/controller/DentistAppController.java
@@ -2,29 +2,35 @@ package com.cgi.dentistapp.controller;
 
 import com.cgi.dentistapp.dto.DentistVisitDTO;
 import com.cgi.dentistapp.dto.SearchQueryDTO;
+import com.cgi.dentistapp.feedback.FeedbackType;
 import com.cgi.dentistapp.service.DentistVisitService;
+import com.cgi.dentistapp.util.FeedbackUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
+import java.util.Locale;
 
 @Controller
 @EnableAutoConfiguration
 public class DentistAppController extends WebMvcConfigurerAdapter {
-
+    private final MessageSource messageSource;
     private final DentistVisitService dentistVisitService;
 
     @Autowired
-    public DentistAppController(DentistVisitService dentistVisitService) {
+    public DentistAppController(MessageSource messageSource,
+                                DentistVisitService dentistVisitService) {
+        this.messageSource = messageSource;
         this.dentistVisitService = dentistVisitService;
     }
 
@@ -39,7 +45,8 @@ public class DentistAppController extends WebMvcConfigurerAdapter {
     }
 
     @PostMapping("/")
-    public String postRegisterForm(@Valid DentistVisitDTO dentistVisitDTO, BindingResult bindingResult) {
+    public String postRegisterForm(@Valid DentistVisitDTO dentistVisitDTO,
+                                   BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "form";
         }
@@ -49,21 +56,27 @@ public class DentistAppController extends WebMvcConfigurerAdapter {
     }
 
     @GetMapping("visits")
-    public String showVisits(Model model){
+    public String showVisits(Model model,
+                             @ModelAttribute("searchQuery") SearchQueryDTO dto) {
         model.addAttribute("dentistVisitDTOs", dentistVisitService.listVisits());
-        model.addAttribute("searchQuery", new SearchQueryDTO());
-        model.addAttribute("searchResult", new ArrayList());
         return "visits";
     }
 
     @PostMapping("visits")
-    public String showSearchResults(@ModelAttribute("searchQuery") @Valid SearchQueryDTO searchQuery, Model model, BindingResult bindingResult){
-        if (bindingResult.hasErrors()){
-            model.addAttribute("searchResult", new ArrayList());
-        }
-        else {
+    public String showSearchResults(@ModelAttribute("searchQuery") @Valid SearchQueryDTO searchQuery,
+                                    BindingResult bindingResult,
+                                    @RequestParam(value = "visit-id", defaultValue = "-1") String visitId,
+                                    Model model,
+                                    Locale locale) {
+
+        if (bindingResult.hasErrors()) {
+            FeedbackUtil.setFeedback(model, FeedbackType.ERROR, messageSource.getMessage("visits.error", null, locale));
+        } else if (visitId.equals("-1")) {
             model.addAttribute("searchResult", dentistVisitService.getSearchResults(searchQuery));
+        } else {
+            return "visit_details";
         }
+
         model.addAttribute("dentistVisitDTOs", dentistVisitService.listVisits());
         return "visits";
     }
